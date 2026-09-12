15a71c86c3ee0c42350973c51d9ed7e89fd33f10
mzn1381 <90798443+mzn1381@users.noreply.github.com>
2026-09-12 22:03:01 +0330

    adding ragflow

--- server/Src/AI/app/chat/nodes/rag_node.py
+++ server/Src/AI/app/chat/nodes/rag_node.py
@@ -1,12 +1,14 @@
 from app.chat.models.models import GraphState
 from app.chat.tools.rag import ApiRagTools
 from app.chat.logger import get_logger
+import uuid
 from app.chat.models.models import GraphState, GraphResult, ResponseType, ToolCallResult
 
 logger = get_logger("inquiry")
 
 _API_MAP = {
     "post_api_sw1_call_rag": ApiRagTools.post_api_sw1_call_rag,
+    "get_api_sw1_call_rag": ApiRagTools.get_api_sw1_call_rag,
 }
 
 _RAG_GENERATION_PROMPT = """
@@ -21,17 +23,49 @@ def rag_node(state: GraphState, llm) -> GraphState:
         #     headers=ApiIrTools.HttpHeader,
         #     json=data,
         # )
+       
+    chat_id = "49debb20aec811f181c1cd529abfa6e1" # Should be refactored #MGZ     
+    conversation_id = "c1616fc0aec911f181c1cd529abfa6e1" # Should be refactored #MGZ     
+    rag_api_name_get = "get_api_sw1_call_rag"
+    func = _API_MAP[rag_api_name_get]    
+    data_get_rag = {"chat_id":chat_id,"conversation_id":conversation_id}
+    history = func.invoke(data_get_rag)
+    messages:list = history["data"]["messages"]
+    
+    messages.append({
+      "content":state.user_input,
+      "conversationId":conversation_id,
+      "files":[],
+      "id":str(uuid.uuid4()),
+      "role":"user"
+      
         
-    rag_api_name = "post_api_sw1_call_rag"
-    func = _API_MAP[rag_api_name]    
-    res = func.invoke(state.user_input)
+    })
+    
+    
+    
+    data_chat_rag={"chat_id":chat_id,"conversation_id":conversation_id,"messages":messages}
+    rag_api_name_post = "post_api_sw1_call_rag"
+    func = _API_MAP[rag_api_name_post] 
+    response=func.invoke(data_chat_rag)
+    ### We are assuming that we are not using streaming right now  == > Should be refactored MGZ
+    res = response[0]["data"] 
+    
+    
+    
+    ({'code': 0, 'data': {...}, 'message': 'success'},)
+    {'answer': 'فردوسی انسانی پرتلاش، صبور و دارای اراده\u200cای قوی بود که زندگی خود را وقف سرودن شاهنامه کرد. او فردی متواضع و بسیار علاقه\u200cمند به حفظ زبان و فرهنگ ایرانی بود و با عشق و انگیزه قوی، بدون حمایت مالی کافی، این اثر عظیم را خلق کرد. او نمادی از وفاداری به هویت ملی و فرهنگی ایران است.', 'audio_binary': None, 'chat_id': '49debb20aec811f181c1cd529abfa6e1', 'created_at': 1789237505.9542406, 'id': 'c50dc5df-a22f-4e38-bab1-dddc04705a74', 'prompt': '', 'reference': {}, 'session_id': 'c1616fc0aec911f181c1cd529abfa6e1'}
+    
     
     #  filling state's essentials    
       
     # state.final_response =res
     # state.response_mgz = GraphResult(toolType=ResponseType.TEXT, text='dddddd')
-    state.response = GraphResult(toolType=ResponseType.TEXT, text=res)
-   
+    # state.response = GraphResult(toolType=ResponseType.TEXT, text=res)
+    return {
+        "response": res["answer"],
+        "final_response": res["answer"],
+    }
     #    
     print("THERE ARE NO ARAD'S APIS  !!!!")
     return state
--- server/Src/AI/app/chat/tools/rag.py
+++ server/Src/AI/app/chat/tools/rag.py
@@ -9,23 +9,57 @@ load_dotenv()
 
 
 class ApiRagTools:
-    BaseUrl = "https://rag.dte.ir/swagger" ## this should be refactor and reviewed
+    BaseUrl = "http://172.16.1.81:8080" ## this should be refactor and reviewed
+    api_key = "ragflow-ubkayYdBo7TsQsUnmorEPSpxPxTPdLBovcCeCE-3yxs"
     HttpHeader = {
         "Content-Type": "application/json",
-        "Authorization": os.getenv("API_DOT_IR_KEY", ""),
+        "Authorization": os.getenv("API_DOT_IR_KEY", api_key),
     }
 
+    
+    
     @tool
-    def post_api_sw1_call_rag(input) -> str:
-        """سرویس کال api در rag service """
-        # data = {"user_name": user_name}
-        # response = requests.post(
-        #     ApiAradTools.BaseUrl + "/api/sw1/post_api_sw1_",
-        #     headers=ApiAradTools.HttpHeader,
-        #     json=data,
-        # )
-        # if response.status_code == 200:
-        #     return "OK\n\n" + json.dumps(response.json(), indent=2, ensure_ascii=False)
-        # return f"Request failed with status code: {response.status_code}"
-        print ("post_api_sw1_ragflow=======> !!!! ")
-        return " Hi there ! "
+    def get_api_sw1_call_rag(chat_id:str,conversation_id:str) -> str:
+            """سرویس کال api در rag service """
+            # data = {"user_name": user_name}
+            response = requests.get(
+                ApiRagTools.BaseUrl + "/api/v1/chats/"+chat_id+"/sessions/"+conversation_id,
+                headers=ApiRagTools.HttpHeader,
+                
+            )
+            if response.status_code == 200:
+               return response.json()
+            return f"Request failed with status code: {response.status_code}"
+    
+    
+    
+    @tool
+    def post_api_sw1_call_rag(chat_id:str,conversation_id:str,messages) -> str:
+            """سرویس کال API در RAG service"""
+    
+            data = {
+            "chat_id": chat_id,
+            "internet": False,
+            "messages":messages,
+            "pass_all_history_messages": True,
+            "reasoning": 1,
+            "stream":False,
+            "session_id": conversation_id
+        }
+    
+            response = requests.post(
+            ApiRagTools.BaseUrl + "/api/v1/chat/completions",
+            headers=ApiRagTools.HttpHeader,
+            json=data,
+        )
+    
+            print("post_api_sw1_ragflow =======> !!!!")
+    
+            if response.status_code == 200:
+                    data=response.json(),
+                    return data
+    
+            return (
+            f"Request failed with status code: {response.status_code}\n"
+            f"Response: {response.text}"
+            )
